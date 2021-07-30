96cba108ae40b51cc5d84017351c808ab4e19505
Bogdan Paratskiy <bogdan.p@didww.com>
2021-07-30 23:16:41 +0300

    Create Courier model

---
+++ spec/models/courier_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe Courier, type: :model do
+  let(:courier) { FactoryBot.create(:courier) }
+
+  it 'email should be saved as lowercase' do
+    courier = FactoryBot.create(:courier, email: 'Foo@ExAMPle.CoM')
+    expect(courier.email).to eq 'foo@example.com'
+  end
+
+  context 'with validation' do
+    it 'should be valid' do
+      expect(courier).to be_valid
+    end
+
+    context 'name' do
+      it 'is invalid without a name' do
+        expect(FactoryBot.build(:courier, name: nil)).not_to be_valid
+      end
+
+      it 'is invalid with too long name' do
+        expect(FactoryBot.build(:courier, name: 'a' * 51)).not_to be_valid
+      end
+    end
+
+    context 'email' do
+      it 'is invalid without an email' do
+        expect(FactoryBot.build(:courier, email: nil)).not_to be_valid
+      end
+
+      it 'does not allow duplicate emails' do
+        expect(FactoryBot.build(:courier, email: courier.email)).not_to be_valid
+      end
+
+      it 'is invalid with too long email' do
+        expect(FactoryBot.build(:courier, email: "#{'a' * 244}@example.com")).not_to be_valid
+      end
+
+      it 'allow mixcase valid email' do
+        valid_emails = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                          first.last@foo.jp alice+bob@baz.cn]
+
+        valid_emails.each do |email|
+          expect(FactoryBot.build(:courier, email: email)).to be_valid
+        end
+      end
+
+      it 'does not allow invalid emails' do
+        invalid_emails = %w[foo@bar..com. user@example,com user_at_foo.org
+                            user.name@example. foo@bar_baz.com foo@bar+baz.com]
+
+        invalid_emails.each do |email|
+          expect(FactoryBot.build(:courier, email: email)).not_to be_valid
+        end
+      end
+    end
+  end
+end
