62928b3987dabf1eac75112bbd405ae7758c7cd9
Bogdan Paratskiy <bogdan.p@didww.com>
2021-09-25 18:45:26 +0300

    US3, Upgade DeliveryManager model test after adding ActiveAdmin/Devise

--- spec/models/delivery_manager_spec.rb
+++ spec/models/delivery_manager_spec.rb
@@ -4,30 +4,47 @@ RSpec.describe DeliveryManager, type: :model do
   describe '#create' do
     subject { DeliveryManager.create(create_params) }
 
-    let(:create_params) { {} }
+    let(:create_params) { FactoryBot.attributes_for(:delivery_manager) }
 
     shared_examples :create_delivery_manager_with_correct_attributes do
+      let(:enabled) { true }
       it 'creates DeliveryManager with correct attributes' do
         expect { subject }.to change { DeliveryManager.count }.by(1)
         expect(subject.errors.messages).to be_empty
-        expect(subject).to have_attributes(
+        expect(DeliveryManager.last).to have_attributes(
           id: be_a_kind_of(String),
           enabled: enabled,
+          email: create_params[:email],
           created_at: be_present,
           updated_at: be_present
         )
       end
     end
 
-    context 'with empty params' do
-      let(:enabled) { true }
+    shared_examples :does_not_create_delivery_manager_with_correct_errors do
+      let(:errors) { nil }
 
+      it 'does not creates DeliveryManager with correct errors' do
+        expect { subject }.to change { DeliveryManager.count }.by(0)
+        expect(subject.errors.full_messages).to match_array(errors)
+      end
+    end
+
+    context 'with required params' do
       it_behaves_like :create_delivery_manager_with_correct_attributes
     end
 
+    context 'with empty params' do
+      let(:create_params) { {} }
+
+      it_behaves_like :does_not_create_delivery_manager_with_correct_errors do
+        let(:errors) { ["Email can't be blank", "Password can't be blank"] }
+      end
+    end
+
     context 'with enabled=false' do
-      let(:create_params) { super().merge(enabled: enabled) }
       let(:enabled) { false }
+      let(:create_params) { super().merge(enabled: enabled) }
 
       it_behaves_like :create_delivery_manager_with_correct_attributes
     end
